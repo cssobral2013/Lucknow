2c6edb7e3408398a6aadc2194e8805c4be20873c
Cristiano Sobral <cssobral2013@gmail.com>
2024-04-28 22:57:16 -0300

    Rebase off of Madhuban & rename to Lucknow

---
+++ generate-woff-woff2.py
@@ -0,0 +1,253 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/Lucknow-ExtraLight.ttf'
+woffPath = './web/Lucknow-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Lucknow-ExtraLightItalic.ttf'
+woffPath = './web/Lucknow-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Lucknow-Light.ttf'
+woffPath = './web/Lucknow-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Lucknow-LightItalic.ttf'
+woffPath = './web/Lucknow-LightItalic.woff'
+   
+
+    
+srcPath  = './ttf/Lucknow-Regular.ttf'
+woffPath = './web/Lucknow-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-Italic.ttf'
+woffPath = './web/Lucknow-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-Medium.ttf'
+woffPath = './web/Lucknow-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-MediumItalic.ttf'
+woffPath = './web/Lucknow-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBold.ttf'
+woffPath = './web/Lucknow-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBoldItalic.ttf'
+woffPath = './web/Lucknow-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBold.ttf'
+woffPath = './web/Lucknow-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBoldItalic.ttf'
+woffPath = './web/Lucknow-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-Bold.ttf'
+woffPath = './web/Lucknow-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-BoldItalic.ttf'
+woffPath = './web/Lucknow-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-ExtraBold.ttf'
+woffPath = './web/Lucknow-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Lucknow-ExtraBoldItalic.ttf'
+woffPath = './web/Lucknow-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Lucknow-ExtraLight.ttf'
+woff2Path = './web/Lucknow-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Lucknow-ExtraLightItalic.ttf'
+woff2Path = './web/Lucknow-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Lucknow-Light.ttf'
+woff2Path = './web/Lucknow-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Lucknow-LightItalic.ttf'
+woff2Path = './web/Lucknow-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+
+    
+srcPath  = './ttf/Lucknow-Regular.ttf'
+woff2Path = './web/Lucknow-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-Italic.ttf'
+woff2Path = './web/Lucknow-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-Medium.ttf'
+woff2Path = './web/Lucknow-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-MediumItalic.ttf'
+woff2Path = './web/Lucknow-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBold.ttf'
+woff2Path = './web/Lucknow-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBoldItalic.ttf'
+woff2Path = './web/Lucknow-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBold.ttf'
+woff2Path = './web/Lucknow-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-SemiBoldItalic.ttf'
+woff2Path = './web/Lucknow-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-Bold.ttf'
+woff2Path = './web/Lucknow-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-BoldItalic.ttf'
+woff2Path = './web/Lucknow-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-ExtraBold.ttf'
+woff2Path = './web/Lucknow-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Lucknow-ExtraBoldItalic.ttf'
+woff2Path = './web/Lucknow-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
